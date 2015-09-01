79ca68ba3291588186298f935fbf132b8cd8d8a5
emg <testeban@gmail.com>
2015-09-01 17:22:04 +0200

    Añadido catalan

--- app/build/generated/source/buildConfig/debug/es/energy/energyotaupdater/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/es/energy/energyotaupdater/BuildConfig.java
@@ -5,7 +5,7 @@ package es.energy.energyotaupdater;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "es.energy.energyotaupdater";
+  public static final String APPLICATION_ID = "es.energy.energyotaupdater";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
--- app/build/generated/source/r/debug/es/energy/energyotaupdater/R.java
+++ app/build/generated/source/r/debug/es/energy/energyotaupdater/R.java
@@ -11,13 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int icon=0x7f020001;
-        public static final int icon_notif=0x7f020002;
+        public static final int icon_not=0x7f020002;
+        public static final int icon_notif=0x7f020003;
+        public static final int illustration_generic=0x7f020004;
+        public static final int illustration_tile=0x7f020005;
+        public static final int welcome_bg=0x7f020006;
     }
     public static final class id {
         public static final int button=0x7f070000;
@@ -32,48 +36,51 @@ public final class R {
         public static final int activity_otaupdater=0x7f030000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int background=0x7f050002;
-        public static final int cancel=0x7f050003;
-        public static final int changelog=0x7f050004;
-        public static final int check_updates=0x7f050005;
-        public static final int click_instalar=0x7f050006;
-        public static final int descarga_finalizada=0x7f050007;
-        public static final int descarga_interrumpida=0x7f050008;
-        public static final int device=0x7f050009;
-        public static final int download=0x7f05000a;
-        public static final int download_error=0x7f05000b;
-        public static final int download_in_progress=0x7f05000c;
-        public static final int downloading=0x7f05000d;
-        public static final int enable_updates=0x7f05001e;
-        public static final int error_searching_updates=0x7f05000e;
-        public static final int hello_world=0x7f05000f;
-        public static final int install=0x7f050010;
-        public static final int install_text=0x7f050011;
-        public static final int install_update=0x7f050012;
-        public static final int last_check=0x7f05001f;
-        public static final int md5_error=0x7f050013;
-        public static final int new_version=0x7f050014;
-        public static final int new_version_found=0x7f050015;
-        public static final int no_updates=0x7f050016;
-        public static final int search=0x7f050017;
-        public static final int searching=0x7f050018;
-        public static final int sin_espacio=0x7f050019;
-        public static final int update_available=0x7f05001a;
-        public static final int update_found=0x7f05001b;
-        public static final int update_not_supported=0x7f05001c;
-        public static final int version=0x7f05001d;
-        public static final int warning=0x7f050020;
+        public static final int accept=0x7f040000;
+        public static final int action_settings=0x7f040001;
+        public static final int app_name=0x7f040002;
+        public static final int background=0x7f040003;
+        public static final int battery_warning=0x7f040004;
+        public static final int battery_warning_dialog=0x7f040005;
+        public static final int cancel=0x7f040006;
+        public static final int changelog=0x7f040007;
+        public static final int check_updates=0x7f040008;
+        public static final int click_instalar=0x7f040009;
+        public static final int descarga_finalizada=0x7f04000a;
+        public static final int descarga_interrumpida=0x7f04000b;
+        public static final int device=0x7f04000c;
+        public static final int download=0x7f04000d;
+        public static final int download_error=0x7f04000e;
+        public static final int download_in_progress=0x7f04000f;
+        public static final int downloading=0x7f040010;
+        public static final int enable_updates=0x7f040011;
+        public static final int error_searching_updates=0x7f040012;
+        public static final int hello_world=0x7f040013;
+        public static final int install=0x7f040014;
+        public static final int install_text=0x7f040015;
+        public static final int install_update=0x7f040016;
+        public static final int last_check=0x7f040017;
+        public static final int md5_error=0x7f040018;
+        public static final int new_version=0x7f040019;
+        public static final int new_version_found=0x7f04001a;
+        public static final int no_updates=0x7f04001b;
+        public static final int search=0x7f04001c;
+        public static final int searching=0x7f04001d;
+        public static final int sin_espacio=0x7f04001e;
+        public static final int update_available=0x7f04001f;
+        public static final int update_found=0x7f040020;
+        public static final int update_not_supported=0x7f040021;
+        public static final int version=0x7f040022;
+        public static final int warning=0x7f040023;
     }
     public static final class style {
-        /** 
+        /**  API 11 theme customizations can go here. 
+ API 14 theme customizations can go here. 
+
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
